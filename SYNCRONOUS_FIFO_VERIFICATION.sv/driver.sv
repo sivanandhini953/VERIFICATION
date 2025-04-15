47b82f4b2c2d32dc120d0d77a7564eeb5a273255
sivanandhini953 <sivanandhinis69@gmail.com>
2025-04-15 09:41:16 +0530

    Create driver.sv

---
+++ SYNCRONOUS_FIFO_VERIFICATION.sv/driver.sv
@@ -0,0 +1,34 @@
+class driver;
+  
+ transaction tr;
+  mailbox gen2drv;
+  virtual intf vif;
+  
+  function new(virtual intf vif,mailbox gen2drv);
+    
+    this.vif=vif;
+    this.gen2drv=gen2drv;
+    
+  endfunction
+  
+  task main();
+    
+    repeat(50) begin
+      
+      tr=new();
+      
+      gen2drv.get(tr);
+      @(posedge vif.clk)
+      
+      vif.rst<=tr.rst;
+      vif.push<=tr.push;
+      vif.pop<=tr.pop;
+      vif.d_in<=tr.d_in;
+      
+      tr.display("DRV");
+      
+    end
+      endtask 
+      
+      endclass
+     
